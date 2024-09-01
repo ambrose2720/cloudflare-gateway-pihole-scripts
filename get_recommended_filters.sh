93faff6b80cbf0b784e82162691a9f5ba84a6d71
ambrose2720 <tiendat200227@gmail.com>
2024-09-01 12:38:23 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 node download_lists.js blocklist
+https://github.com/AdguardTeam/AdguardSDNSFilter
+https://adguard.com/kb/general/ad-filtering/adguard-filters/#mobile-ads-filter
+https://github.com/bigdargon/hostsVN
+https://easylist.to
+https://github.com/StevenBlack/hosts
+https://github.com/AdAway/adaway.github.io
+https://pgl.yoyo.org/adservers
+https://abpvn.com/
