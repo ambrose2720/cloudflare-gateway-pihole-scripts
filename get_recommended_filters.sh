c01c1ebe6082022c176f5165d3410356453d17ca
ambrose2720 <tiendat200227@gmail.com>
2024-09-02 00:18:13 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -8,4 +8,6 @@ https://easylist.to
 https://github.com/StevenBlack/hosts
 https://github.com/AdAway/adaway.github.io
 https://pgl.yoyo.org/adservers
-https://abpvn.com/
+https://abpvn.com
+https://raw.githubusercontent.com/nmtrung/FMSF-2.0/master/fmsf_2.0.txt
+https://raw.githubusercontent.com/bigdargon/hostsVN/master/extensions/gambling/hosts
