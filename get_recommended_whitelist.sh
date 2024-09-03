2acafe2335d349fa719ddc0720bc4bc127f98e0e
ambrose2720 <tiendat200227@gmail.com>
2024-09-04 00:04:01 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 node download_lists.js allowlist
+https://raw.githubusercontent.com/nextdns/click-tracking-domains/main/domains
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
