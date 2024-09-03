daf84e1de1b79a52e3253921c3200caaa16852c4
ambrose2720 <tiendat200227@gmail.com>
2024-09-03 22:38:03 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 node download_lists.js blocklist
-https://github.com/AdguardTeam/AdguardSDNSFilter
-https://adguard.com/kb/general/ad-filtering/adguard-filters/#mobile-ads-filter
-https://github.com/bigdargon/hostsVN
-https://easylist.to
-https://github.com/StevenBlack/hosts
-https://github.com/AdAway/adaway.github.io
-https://pgl.yoyo.org/adservers
-https://abpvn.com
+
+
+https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
+
+https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
+https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt
+https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts
+https://abpvn.com/filter/abpvn-klbKBc.txt
 https://raw.githubusercontent.com/nmtrung/FMSF-2.0/master/fmsf_2.0.txt
 https://raw.githubusercontent.com/bigdargon/hostsVN/master/extensions/gambling/hosts
